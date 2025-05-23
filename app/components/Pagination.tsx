ec553b4bcabf4906c4231e6dd8ae3f279710b510
Angelo Ilardi <ange448@gmail.com>
2025-05-23 17:45:29 +0100

    pagintion improvements

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -27,12 +27,22 @@ const Pagination: React.FC<PaginationProps> = ({
 
   const renderPageNumbers = () => {
     const pages = [];
-    for (let i = 1; i <= totalPages; i++) {
+    const maxPagesToShow = 10;
+    let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+    const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+
+    if (endPage - startPage + 1 < maxPagesToShow) {
+      startPage = Math.max(1, endPage - maxPagesToShow + 1);
+    }
+
+    for (let i = startPage; i <= endPage; i++) {
       pages.push(
         <button
           key={i}
           onClick={() => onPageChange(i)}
-          className={i === currentPage ? "border-b border-b-accent" : ""}
+          className={
+            i === currentPage && endPage > 1 ? "border-b border-b-accent" : ""
+          }
         >
           {i}
         </button>
@@ -42,12 +52,12 @@ const Pagination: React.FC<PaginationProps> = ({
   };
 
   return (
-    <div className="flex gap-4">
-      <div className="flex items-center gap-2">
-        <span className="text-gray-600">
-          Showing {(currentPage - 1) * 10 + 1} -{" "}
-          {Math.min(currentPage * 10, totalItems)} of {totalItems} results
-        </span>
+    <div className="flex gap-4 flex-col">
+      <span className="text-gray-600">
+        Showing {(currentPage - 1) * 10 + 1} -{" "}
+        {Math.min(currentPage * 10, totalItems)} of {totalItems} results
+      </span>
+      <div className="flex gap-4">
         <button
           onClick={handlePrevious}
           disabled={currentPage === 1}
